25d36bb3e462e362601a2a580c5b40b024dfccae
keldykins <kelcundy@gmail.com>
2020-11-17 21:24:33 -0700

    added pupster reference files to refactor

---
+++ spiral/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Pupster 2017</span>
+    </footer>
+  );
+}
+
+export default Footer;
