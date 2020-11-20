15a810e692da33a4328a2bef7b340f08f0d210f1
gbluemd <gsjujub@gmail.com>
2020-11-19 19:48:14 -0700

    2nd try

---
+++ Spiral_V3/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/css/App.css';
+import HeroSection from '../HeroSection';
+import footer from '../footer';
+
+function Home () {
+    return (
+        <>
+        <HeroSection />
+        <footer />
+        </>
+    );
+}
+
+export default Home;
---
+++ Spiral_V3/src/components/pages/logIn.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../assets/css/App.css';
+
+
+export default function logIn () {
+    return <h1 className='login'>Log In</h1>
+}
