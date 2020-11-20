36a25b0a207605448109546ada09fa3b90b95aec
Maynard Peralta <maynperalta@gmail.com>
2020-11-19 21:03:46 -0700

    added backend files

---
+++ controllers/post.js
@@ -0,0 +1,19 @@
+const Post = require("../models/post");
+
+exports.getPosts = (req, res) => {
+  const posts = Post.find()
+    .select("_id title body")
+    .then((posts) => {
+      res.json({ posts });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.createPost = (req, res) => {
+  const post = new Post(req.body);
+  post.save().then((result) => {
+    res.status(200).json({
+      post: result,
+    });
+  });
+};
