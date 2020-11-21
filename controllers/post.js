fa322d74f4a8c1c8160975108be1f3b8e36db55b
Maynard Peralta <maynperalta@gmail.com>
2020-11-21 09:15:17 -0700

    added apiDocs file

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,7 @@
 const Post = require("../models/post");
 const formidable = require("formidable");
 const fs = require("fs");
+const _ = require("lodash");
 
 exports.postById = (req, res, next, id) => {
   Post.findById(id)
@@ -12,6 +13,7 @@ exports.postById = (req, res, next, id) => {
         });
       }
       req.post = post;
+      // Continue to next middleware.
       next();
     });
 };
@@ -79,6 +81,20 @@ exports.isPoster = (req, res, next) => {
     });
   }
   next();
+};
+
+exports.updatePost = (req, res, next) => {
+  let post = req.post;
+  post = _.extend(post, req.body);
+  post.updated = Date.now();
+  post.save((err) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json(post);
+  });
 };
 
 exports.deletePost = (req, res) => {
