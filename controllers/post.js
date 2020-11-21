168ea0c16812f9028c5a192597b4dc2af7d0a11f
Maynard Peralta <maynperalta@gmail.com>
2020-11-21 01:14:00 -0700

    added delete route

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,24 @@
 const Post = require("../models/post");
+const formidable = require("formidable");
+const fs = require("fs");
+
+exports.postById = (req, res, next, id) => {
+  Post.findById(id)
+    .populate("postedBy", "_id name")
+    .exec((err, post) => {
+      if (err || !post) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+      req.post = post;
+      next();
+    });
+};
 
 exports.getPosts = (req, res) => {
   const posts = Post.find()
+    .populate("postedBy", "_id name")
     .select("_id title body")
     .then((posts) => {
       res.json({ posts });
@@ -9,11 +26,71 @@ exports.getPosts = (req, res) => {
     .catch((err) => console.log(err));
 };
 
-exports.createPost = (req, res) => {
-  const post = new Post(req.body);
-  post.save().then((result) => {
-    res.status(200).json({
-      post: result,
+exports.createPost = (req, res, next) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded.",
+      });
+    }
+    let post = new Post(fields);
+    // Set hashed_password and salt to show as undefined
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    // Handle file uploaded by user
+    post.postedBy = req.profile;
+    if (files.photo) {
+      post.photo.data = fs.readFileSync(files.photo.path);
+      post.photo.contentType = files.photo.type;
+    }
+    // Save the post
+    post.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+      res.json(result);
+    });
+  });
+};
+
+exports.postsByUser = (req, res) => {
+  Post.find({ postedBy: req.profile._id })
+    .populate("postedBy", "_id name")
+    .sort("_created")
+    .exec((err, posts) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+      res.json(posts);
+    });
+};
+
+exports.isPoster = (req, res, next) => {
+  let isPoster = req.post && req.auth && req.post.postedBy._id == req.auth._id;
+  if (!isPoster) {
+    return res.status(403).json({
+      error: "User is not authorized to perform this action.",
+    });
+  }
+  next();
+};
+
+exports.deletePost = (req, res) => {
+  let post = req.post;
+  post.remove((err, post) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json({
+      message: "Post deleted.",
     });
   });
 };
--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getPosts, createPost } = require("../controllers/post");
+const { getPosts, createPost, postsByUser, postById, isPoster, deletePost } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const { createPostValidator } = require("../validator");
@@ -7,10 +7,14 @@ const { createPostValidator } = require("../validator");
 const router = express.Router()
 
 router.get("/", getPosts);
-router.post("/post", requireSignin, createPostValidator, createPost);
+router.post("/post/new/:userId", requireSignin, createPost, createPostValidator);
+router.get("/posts/by/:userId", requireSignin, postsByUser)
+router.delete("/post/:postId", requireSignin, isPoster, deletePost)
 
 // Any route containing :userId, App will first execute userById()
 router.param("userId", userById);
+// Any route containing :postId, App will first execute postById()
+router.param("postId", postById);
 
 module.exports = router;
 
