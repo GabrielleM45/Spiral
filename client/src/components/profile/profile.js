0863e13ecbfe0ded225f8adaf9d426f9cea1d9fa
unknown <metybuza46@gmail.com>
2020-11-20 19:43:32 -0700

    updated client file

---
+++ client/src/components/profile/profile.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+
+
+      profileContent = (<div className="profile">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="row">
+              <div className="col-6">
+                <a href="profiles.html" className="btn btn-light mb-3 float-left">Back To Profiles</a>
+              </div>
+              <div className="col-6"></div>
+           </div>
+           {/*Profile Header*/}
+           {/* <ProfileHeader profile={profile} /> */}
+           {/*Profile About*/}
+           {/* <ProfileAbout profile={profile} /> */}
+           {/*Profile Credentials*/}
+           {/* <ProfileCred profile={profile} /> */}
+           {/*Profile Github*/}
+           {/* {profile.githubusername ? (<ProfileGithub profile={profile} />) : null} */}
+           
+           
+        </div>
+      </div>
+    </div>
+   </div>  );
+    
+
+
+export default Profile;
