3023519f0e9b56790cb88c3adf9e6dcee83bb399
Maynard Peralta <maynperalta@gmail.com>
2020-11-29 11:01:09 -0700

    added function to update posts

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getPosts, createPost, postsByUser, postById, isPoster, deletePost } = require("../controllers/post");
+const { getPosts, createPost, postsByUser, postById, isPoster, updatePost, deletePost } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const { createPostValidator } = require("../validator");
@@ -9,7 +9,9 @@ const router = express.Router()
 router.get("/", getPosts);
 router.post("/post/new/:userId", requireSignin, createPost, createPostValidator);
 router.get("/posts/by/:userId", requireSignin, postsByUser);
-router.delete("/post/:postId", requireSignin, isPoster, deletePost)
+router.put("/post/:postId", requireSignin, isPoster, updatePost)
+router.delete("/post/:postId", requireSignin, isPoster, deletePost);
+
 
 // Any route containing :userId, App will first execute userById()
 router.param("userId", userById);
