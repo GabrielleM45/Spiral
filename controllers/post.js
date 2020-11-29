c51b924c5bada83e240c893744fd5f5a86819a86
Maynard Peralta <maynperalta@gmail.com>
2020-11-29 10:33:51 -0700

    added function to get posts by user

--- controllers/post.js
+++ controllers/post.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 
 exports.getPosts = (req, res) => {
   const posts = Post.find()
+    .populate("postedBy", "_id name")
     .select("_id title body")
     .then((posts) => {
       res.json({ posts });
@@ -37,5 +38,20 @@ exports.createPost = (req, res, next) => {
       }
       res.json(result);
     });
+  });
+};
+
+// Find posts by user
+exports.postsByUser = (req, res) => {
+  Post.find({postedBy: req.profile._id})
+  .populate("postedBy", "_id name")
+  .sort("_created")
+  .exec((err, posts) => {
+    if (err) {
+      return res.status(400).json({
+        error: err
+      });
+    }
+    res.json(posts);
   });
 };
--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getPosts, createPost } = require("../controllers/post");
+const { getPosts, createPost, postsByUser } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const { createPostValidator } = require("../validator");
@@ -8,6 +8,7 @@ const router = express.Router()
 
 router.get("/", getPosts);
 router.post("/post/new/:userId", requireSignin, createPost, createPostValidator);
+router.get("/posts/by/:userId", requireSignin, postsByUser)
 
 // Any route containing :userId, App will first execute userById()
 router.param("userId", userById);
