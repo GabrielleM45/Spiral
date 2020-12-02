068fcd408d411e84179db54ae52492317e6977c5
keldykins <kelcundy@gmail.com>
2020-12-01 20:06:01 -0700

    update

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import '../assets/css/App.css';
-import LandingSection from '../LandingSection';
-import footer from '../footer';
+import React from "react";
+import "../assets/css/App.css";
+import LandingSection from "../LandingSection";
+// import Footer from '../Footer';
 
-function Home () {
-    return (
-        <>
-        <LandingSection />
-        <footer />
-        </>
-    );
+function Home() {
+  return (
+    <>
+      <LandingSection />
+      {/* <Footer /> */}
+    </>
+  );
 }
 
 export default Home;
