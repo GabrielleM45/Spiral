e69a16ed6f4956ad785ff98f5e2424f8fe027c28
keldykins <kelcundy@gmail.com>
2020-12-05 13:19:39 -0700

    deploying

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
   .then(() => console.log("DB Connected."));
 
@@ -30,21 +30,21 @@ const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 // apiDocs
-app.get("/", (req, res) => {
-  fs.readFile("docs/apiDocs.json", (err, data) => {
-    if (err) {
-      res.status(400).json({
-        error: err,
-      });
-    }
-    const docs = JSON.parse(data);
-    res.json(docs);
-  });
-});
+// app.get("/", (req, res) => {
+//   fs.readFile("docs/apiDocs.json", (err, data) => {
+//     if (err) {
+//       res.status(400).json({
+//         error: err,
+//       });
+//     }
+//     const docs = JSON.parse(data);
+//     res.json(docs);
+//   });
+// });
 
 // if production env utilize build folder and render react app
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
 // Middleware
@@ -68,4 +68,3 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`A Node JS API is listening on port: ${port}`);
 });
-
