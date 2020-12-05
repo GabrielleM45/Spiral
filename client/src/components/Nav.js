bd2fbf5dd9979aff107ae2dc1337188ed72a54d5
Maynard Peralta <maynperalta@gmail.com>
2020-12-05 11:03:25 -0700

    fixed navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -11,11 +11,12 @@ const Nav = ({ history }) => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav`" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar nav">
+  <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar nav ml-auto">
         <li className="nav-item">
           <Link className="nav-link" style={isActive(history, "/")} to="/">
             Home
