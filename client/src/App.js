4d6c464e3964a4b7706510f270ebb1d56555e954
Maynard Peralta <maynperalta@gmail.com>
2020-12-07 18:08:44 -0700

    changed router to hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import MainRouter from "./routes/MainRouter";
 import "./App.css"
 
 const App = () => {
   return (
-  <BrowserRouter>
+  <HashRouter basename = "/">
     <MainRouter />
-  </BrowserRouter>
+  </HashRouter>
   )
 }
 export default App;
